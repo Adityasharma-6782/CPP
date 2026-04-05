2e7a54e8cf21fe84aafdda6084941420aad5c8dc
Aditya Sharma <sharmaadi6782@gmail.com>
2026-04-05 11:30:18 +0530

    Refactor array swapping implementation

--- Day02/01_AlternetSwaping.cpp
+++ Day02/01_AlternetSwaping.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/*
-void SwappingArray(int arr[], int n)       
-{
-    for (int i = 0; i < (n - 1); i++)
-    {
-        swap(arr[i], arr[++i]);
-    }
-    cout << endl;
-}
-*/
-
 void SwappingArray(int arr[], int n)
 {
     for (int i = 0; i < n - 1; i += 2)
